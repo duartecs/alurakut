354b64a27f9056c2dcb2d4e29cfa0e96fa171452
ismagaia <matheusduartec96@gmail.com>
2021-07-13 00:17:56 -0300

    Inicio do Alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,58 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import {AlurakutMenu, OrkutNostalgicIconSet} from '../src/lib/AlurakutCommons'
+import {ProfileRelationsBoxWrapper} from '../src/components/ProfileRelations'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar(props){
+  return(
+    <Box className="profileArea">
+      <img src={`https://github.com/${props.githubUser}.png`} style={{borderRadius: '8px'}}/>
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+
+  const githubUser = 'duartecs';
+  const pessoasFavoritas = ['juunegreiros', 'omariosouto', 'peas', 'rafaballerini', 'marcobrunodev', 'felipefialho']
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div style={{gridArea: "profileArea"}}>
+          <ProfileSidebar githubUser={githubUser}/>
+        </div>
+        <div style={{gridArea: "welcomeArea"}}>
+          <Box className="welcomeArea">
+            <h1 className="title">Bem vindo(a)</h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div style={{gridArea: "profileRelationsArea"}}> 
+          <ProfileRelationsBoxWrapper>
+
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({pessoasFavoritas.length})
+            </h2>
+            <ul>
+              {pessoasFavoritas.map((item)=>{
+                return (
+                  <li key={item}>
+                    <a href={`/users/${item}`}>
+                      <img src={`https://github.com/${item}.png`} />
+                      <span>{item}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <Box className="profileRelationsArea">
+            Comunidades
+          </Box>
+        </div>
+      </MainGrid>
+    </>
+  )
 }
