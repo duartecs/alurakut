4f58eaea49ed2f680c21122f3c8fa2ea3b63a99d
ismagaia <matheusduartec96@gmail.com>
2021-07-18 19:20:21 -0300

    Finalizacao do alurakut

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ import {
 } from "../src/lib/AlurakutCommons";
 import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
 import { useEffect, useState } from "react";
+import nookies from "nookies";
+import jwt from "jsonwebtoken";
 
 function ProfileSidebar(props) {
   return (
@@ -50,8 +52,8 @@ function ProfileRelationsBox(props) {
   );
 }
 
-export default function Home() {
-  const githubUser = "duartecs";
+export default function Home(props) {
+  const githubUser = props.githubUser;
   const [comunidades, setComunidades] = useState([]);
   const pessoasFavoritas = [
     "juunegreiros",
@@ -192,4 +194,33 @@ export default function Home() {
       </MainGrid>
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context);
+  const token = cookies.USER_TOKEN;
+
+  const { isAuthenticated } = await fetch(
+    "https://alurakut.vercel.app/api/auth",
+    {
+      headers: {
+        Authorization: token,
+      },
+    }
+  ).then((resposta) => resposta.json());
+
+  if (!isAuthenticated) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+  const { githubUser } = jwt.decode(token);
+  return {
+    props: {
+      githubUser,
+    },
+  };
 }
